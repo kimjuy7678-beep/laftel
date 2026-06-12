0e316ec8d7f9a6839d56a547121734c602f55437
sungmin <okok0216@gmail.com>
2026-06-12 18:00:31 +0900

    conflict

--- app/api/laftel/[...path]/route.ts
+++ app/api/laftel/[...path]/route.ts
@@ -1,13 +1,13 @@
 import { NextRequest, NextResponse } from 'next/server'
 
-const BASE = 'https://api.laftel.net/api/events/v2'
+const BASE = 'https://api.laftel.net/api'
 
 export async function GET(req: NextRequest, { params }: { params: Promise<{ path: string[] }> }) {
     const { path } = await params
     const pathStr = path.join('/')
     const { searchParams } = new URL(req.url)
     const query = searchParams.toString()
-    const url = `${BASE}/${pathStr}/${query ? '?' + query : ''}`
+    const url = `${BASE}/${pathStr}${query ? '?' + query : ''}`
     try {
         const res = await fetch(url, { headers: { 'Content-Type': 'application/json' } })
         const data = await res.json()
